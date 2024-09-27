be50d56028e08cef33b3652cd6bb65c91394ca12
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-09-27 10:08:02 +0100

    move code to Collections Extensions

--- Far.Library/StringIndexFinder.cs
+++ Far.Library/StringIndexFinder.cs
@@ -16,7 +16,12 @@
  */
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using AlastairLundy.Extensions.Collections.IEnumerables;
+
+using AlastairLundy.Extensions.System.Indexes;
 
 using Far.Library.Abstractions;
 
@@ -24,108 +29,31 @@ namespace Far.Library;
 
 public class StringIndexFinder : IStringIndexFinder
 {
-    /// <summary>
-    /// Gets the indexes of the specified string within a string.
-    /// </summary>
-    /// <param name="strings">The IEnumerable of strings to be searched.</param>
-    /// <param name="expected">The string to look for.</param>
-    /// <param name="ignoreCase">Whether to ignore the case of the expected string.</param>
-    /// <returns>The indexes if the string is found.</returns>
-    public IEnumerable<int> GetStringIndex(IEnumerable<string> strings, string expected, bool ignoreCase)
-    {
-        List<int> indexes = new List<int>();
 
-        int currentCharIndex = 0;
-        
-        foreach (string str in strings)
-        {
-            if (str.Split(' ').Length > 0)
-            {
-                string[] words = str.Split(' ');
-
-                foreach (string word in words)
-                {
-                    if(word.Equals(expected, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal))
-                    {
-                        indexes.Add(currentCharIndex);
-                    }
-
-                    foreach (char unused in word)
-                    {
-                        currentCharIndex++;
-                    }
-                }
-            }
-            else
-            {
-                if (str.Contains(expected))
-                {
-                    if (ignoreCase)
-                    {
-                        indexes.Add(currentCharIndex + str.IndexOf(expected, StringComparison.OrdinalIgnoreCase));  
-                    }
-                    else
-                    {
-                        indexes.Add(currentCharIndex + str.IndexOf(expected, StringComparison.Ordinal));
-                    }
-                }
-                
-                foreach (char unused in str)
-                {
-                    currentCharIndex++;
-                }
-            }
-        }
-        
-        return indexes;
-    }
 
     /// <summary>
-    /// Gets the indexes of the specified string within a string.
+    /// 
     /// </summary>
-    /// <param name="toBeSearched">The string to be searched.</param>
-    /// <param name="expected">The string to look for.</param>
-    /// <param name="ignoreCase">Whether to ignore the case of the expected string.</param>
-    /// <returns>The indexes if the string is found.</returns>
-    public IEnumerable<int> GetStringIndex(string toBeSearched, string expected, bool ignoreCase)
+    /// <param name="toBeSearched"></param>
+    /// <param name="expected"></param>
+    /// <param name="ignoreCase"></param>
+    /// <returns></returns>
+    public IEnumerable<int> GetStringIndexes(string toBeSearched, string expected, bool ignoreCase)
     {
-        return GetStringIndex(new string[]{toBeSearched}, expected, ignoreCase);
+        return toBeSearched.IndexesOf(expected, ignoreCase);
     }
-    
-    /// <summary>
-    /// Gets the indexes of the specified char in an IEnumerable of strings.
-    /// </summary>
-    /// <param name="strings">The IEnumerable of strings to be searched.</param>
-    /// <param name="expected">The char to look for.</param>
-    /// <param name="ignoreCase">Whether to ignore the case of the expected char.</param>
-    /// <returns>The indexes if the char is found.</returns>
-    public IEnumerable<int> GetCharIndex(IEnumerable<string> strings, char expected, bool ignoreCase)
+
+    public IEnumerable<int> GetStringIndexes(IEnumerable<string> toBeSearched, string expected, bool ignoreCase)
     {
-        List<int> indexes = new List<int>();
-
-        int currentIndex = 0;
-
-        foreach (string str in strings)
-        {
-            foreach (char c in str)
-            {
-                if (c == expected)
-                {
-                    if (ignoreCase ||
-                        string.Equals(c.ToString(), expected.ToString(), StringComparison.InvariantCulture) ||
-                        string.Equals(c.ToString(), expected.ToString(), StringComparison.InvariantCulture))
-                    {
-                        indexes.Add(currentIndex);
-                    }
-                }
-            
-                currentIndex++;
-            }
-        }
-        
-        return indexes;
+        return toBeSearched.IndexesOf(expected, ignoreCase);
     }
-    
+
+    public IEnumerable<int> GetCharIndexes(IEnumerable<string> toBeSearched, char expected, bool ignoreCase)
+    {
+        return toBeSearched.IndexesOf(expected, ignoreCase);
+    }
+
+
     /// <summary>
     /// Gets the indexes of the specified char in a string.
     /// </summary>
@@ -133,8 +61,8 @@ public class StringIndexFinder : IStringIndexFinder
     /// <param name="expected">The char to look for.</param>
     /// <param name="ignoreCase">Whether to ignore the case of the expected char.</param>
     /// <returns>The indexes if the char is found.</returns>
-    public IEnumerable<int> GetCharIndex(string toBeSearched, char expected, bool ignoreCase)
+    public IEnumerable<int> GetCharIndexes(string toBeSearched, char expected, bool ignoreCase)
     {
-       return GetCharIndex(new string[] { toBeSearched }, expected, ignoreCase);
+        return toBeSearched.IndexesOf(expected, ignoreCase);
     }
 }
