a446ccbf34f70ef0621799f3264e982804ff930f
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-09-27 13:02:40 +0100

    remove char replacement code

--- Far.Library/Abstractions/Replacers/ICharReplacer.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Far.Library.Abstractions.Replacers;
-
-public interface ICharReplacer
-{
-    IEnumerable<string> ReplaceCharacter(IEnumerable<string> enumerable, char toBeReplaced, char replacementChar);
-}
